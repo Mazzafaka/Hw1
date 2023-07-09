6af58ffac3018db8a7fb2b2669d7c930428700eb
Zack <1984odina.gmail.com>
2023-07-09 21:30:33 +0600

    Added all

---
+++ Home004/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 8:    
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine());
+if(N % 2 == 0){
+    Console.WriteLine("Ваше число чётное");
+}
+else{
+    Console.WriteLine("Ваше число не чётное");
+}
